61dfb55924a26238b882772f31894e88e903f0ba
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-05-21 01:25:18 +0530

    Added landing page

--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,3 +1,20 @@
-// export default function Landing(){
-//     return()
-// }
+import { NavLink } from "react-router-dom";
+import { useFetchContext } from "../../contexts/FetchContext"
+import "./Landing.css"
+export default function Landing(){
+    const {allData} = useFetchContext()
+    return(<div className="container"> 
+    {
+        allData.arrCategories.map((item)=>{
+            const {_id,categoryName,image,description} = item;
+            return(<div className="circle" key={_id}>
+                <img className="pic" src={`${image}`} width="100%" height="160px" alt="" />
+                <NavLink to="../pages/ProductPage/Product" className="heading">{categoryName}</NavLink>
+            </div>
+
+        )
+    })
+}
+    <img src="https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/N2GLoffers/cashback-pc._CB590089559_.gif" width="100%" height="300px" style={{marginTop:"30px"}}></img>
+</div>)
+}
