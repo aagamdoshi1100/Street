75835af82cd81ddf181aa89f2227469a18f84b8d
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-05-21 01:25:57 +0530

    Added style

--- src/pages/NavBarPage/NavBar.jsx
+++ src/pages/NavBarPage/NavBar.jsx
@@ -2,10 +2,9 @@ import "./NavBar.css"
 import {NavLink} from "react-router-dom"
 export default function NavBar(){
     return(<div className="nav-header">
-      <ul className="navbar">
         <div className="navbar-main">
             <div className="navbar-left">
-                <NavLink>
+                <NavLink className="link">
                     <h2>AD</h2>
                 </NavLink>
             </div>
@@ -13,10 +12,9 @@ export default function NavBar(){
                 <input type="text" className="search-bar" placeholder="Search for products" />
             </div>
             <ul className="navbar-right">
-               <NavLink to="#">Cart</NavLink>
-               <NavLink to="#">Wishlist</NavLink>
+               <NavLink className="link" to="#">Cart</NavLink>
+               <NavLink className="link"to="#">Wishlist</NavLink>
             </ul>
         </div>
-      </ul>
         </div>)
 }
