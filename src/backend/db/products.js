3e62d5a7da97c48e53dae1b5df18a3f264b53fa4
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-05-21 01:43:15 +0530

    Added products

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -17,7 +17,14 @@ export const products = [
     _id: uuid(),
     title: "Bigbanana Plus Size Fit Polo",
     image: "https://m.media-amazon.com/images/I/61UDal8a8oL._UY550_.jpg",
-    price: "999",
+    price: "1399",
+    Material: "Cotton",
+  },
+  {
+    _id: uuid(),
+    title: "Women's Cotton Kurta",
+    image: "https://m.media-amazon.com/images/I/81QN4uIauwL._AC_UL400_.jpg",
+    price: "1999",
     Material: "Cotton",
   },
   {
@@ -25,6 +32,12 @@ export const products = [
     title: "AMERICAN Collar T-Shirt Men",
     image: "https://m.media-amazon.com/images/I/312kBG9174L.jpg",
     price: "1299",
+    Material: "Cotton",
+  },  {
+    _id: uuid(),
+    title: "Girls Cotton T-Shirt - Combo of 5 (KUC-TSHRT-0143)",
+    image: "https://m.media-amazon.com/images/I/81eMNadh9EL._AC_UL400_.jpg",
+    price: "999",
     Material: "Cotton",
   },
   {
@@ -36,11 +49,25 @@ export const products = [
   },
   {
     _id: uuid(),
+    title: "Women's Synthetic Kurta",
+    image: "https://m.media-amazon.com/images/I/81ye3FMqvoL._AC_UL400_.jpg",
+    price: "999",
+    Material: "Cotton",
+  },
+  {
+    _id: uuid(),
     title: "Veirdo Polo T Shirts for Men",
     image: "https://m.media-amazon.com/images/I/51i84HTi9kL._UX569_.jpg",
     price: "699",
     Material: "Cotton",
   },
+  {
+    _id: uuid(),
+    title: "Bigbanana Plus Size Fit Polo",
+    image: "https://m.media-amazon.com/images/I/61UDal8a8oL._UY550_.jpg",
+    price: "799",
+    Material: "Cotton",
+  }, 
   {
     _id: uuid(),
     title: "LEOTUDE Men's Half Sleeve",
@@ -57,9 +84,17 @@ export const products = [
   },
   {
     _id: uuid(),
-    title: "LEOTUDE Half Sleeve Men's T-Shirt",
-    image: "https://m.media-amazon.com/images/I/41HY6OkR8CL._UY500_.jpg",
+    title: "Women's Synthetic Straight Kurta",
+    image: "https://m.media-amazon.com/images/I/81laipB1Z3L._AC_UL400_.jpg",
     price: "499",
+    Material: "Cotton",
+  },
+  
+  {
+    _id: uuid(),
+    title: "Boys T-Shirt",
+    image: "https://m.media-amazon.com/images/I/A1HtatAa1-S._AC_UL400_.jpg",
+    price: "259",
     Material: "Cotton",
   },
 ];
