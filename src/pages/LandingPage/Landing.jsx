762a32765cb7ad133ce084feb3ae25e321da52b3
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-20 13:16:17 +0530

    style: landing page for desktop view

--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,25 +1,29 @@
 import { NavLink } from "react-router-dom";
 import { useFetchContext } from "../../contexts/FetchContext"
 import "./Landing.css"
-export default function Landing(){
-    const {productState} = useFetchContext()
-    return(<div className="container"> 
-   
-    {
-        productState.arrCategories.map((item)=>{
-            const {_id,categoryName,image,description} = item;
-            return(<div className="circle" key={_id}>
-                <img className="pic" src={`${image}`} width="100%" height="160px" alt="" />
-                <NavLink to="../pages/ProductPage/Product" className="heading">{categoryName}</NavLink>
-            </div>
+export default function Landing() {
+    const { productState } = useFetchContext()
+    return (<div className="categories-container">
+        <div className="categories">
 
-        )
-    })
-    
-}
+            {
+                productState.arrCategories.map((item) => {
+                    const { _id, categoryName, image, description } = item;
+                    return (<div className="circle" key={_id}>
+                        <img className="pic" src={`${image}`} width="100%" height="100%" alt="" />
+                        <div className="heading">
+                            <NavLink to="../pages/ProductPage/Product" className="heading-name">{categoryName}</NavLink>
+                        </div>
+                    </div>
 
-{/* <div> <img src="https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/N2GLoffers/cashback-pc._CB590089559_.gif" width="100%" height="300px" style={{marginTop:"30px"}}></img>
-    </div> */}
-   
-</div>)
+                    )
+                })
+
+            }
+        </div>
+        <div>
+            <img src="https://shorturl.at/cxPT1" width="100%" height="100%" />
+        </div>
+
+    </div>)
 }
