59beec12d67e8cd458accc978a88545cb5ff677f
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-21 12:11:12 +0530

    navigatation added

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,51 +1,68 @@
-import { useState } from "react"
-import { useContext } from "react"
-import { createContext } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-export function AuthContextProvider({children}){
-    const [isLoggedIn , setIsloggedIn] = useState(false)
-     
-    const navigate = useNavigate()
-    
+export function AuthContextProvider({ children }) {
+  const [isLoggedIn, setIsloggedIn] = useState(false);
 
-    const createAcc =async(mail,pass,user)=>{
-        try{
-        const res = await fetch("/api/auth/signup",{method:"POST",body:JSON.stringify({email :mail,password:pass,someUserAttribute1:user})})
-        navigate("/pages/Authentication/Login")
-        }catch(e){
-            console.log("🚀 ~ file: AuthContext.js:15 ~ createAcc ~ e:", e)
-            
-        }
+  const navigate = useNavigate();
+
+  const createAcc = async (mail, pass, user) => {
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        body: JSON.stringify({
+          email: mail,
+          password: pass,
+          someUserAttribute1: user,
+        }),
+      });
+      navigate("/pages/Authentication/Login");
+    } catch (e) {
+      console.log("🚀 ~ file: AuthContext.js:15 ~ createAcc ~ e:", e);
     }
+  };
 
-    const setTokenToLocalStorage = async(user,pass)=>{
-        console.log("🚀 ~ file: AuthContext.js:11 ~ setTokenToLocalStorage ~ user,pass:", user,pass)
-        try{
-            const cred=  {
-                email: user,
-                password: pass
-            }       
-            const restoken = await fetch("/api/auth/login",{
-                method: "POST",
-                body : JSON.stringify(cred)
-            })
-            const {encodedToken} =  await restoken.json() 
-            localStorage.setItem("encodedToken",encodedToken)
-            setIsloggedIn(true)
-        }
-        catch(e){
-            console.log(e)
-        }
+  const setTokenToLocalStorage = async (user, pass) => {
+    console.log(
+      "🚀 ~ file: AuthContext.js:11 ~ setTokenToLocalStorage ~ user,pass:",
+      user,
+      pass
+    );
+    try {
+      const cred = {
+        email: user,
+        password: pass,
+      };
+      const restoken = await fetch("/api/auth/login", {
+        method: "POST",
+        body: JSON.stringify(cred),
+      });
+      const { encodedToken } = await restoken.json();
+      localStorage.setItem("encodedToken", encodedToken);
+      setIsloggedIn(true);
+    } catch (e) {
+      console.log(e);
     }
-    
-    return(<AuthContext.Provider value={{isLoggedIn,setTokenToLocalStorage,setIsloggedIn,createAcc}}>
-        {children}</AuthContext.Provider>)
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        setTokenToLocalStorage,
+        setIsloggedIn,
+        createAcc,
+        navigate,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-
-const useAuthContext =()=> useContext(AuthContext)
+const useAuthContext = () => useContext(AuthContext);
 export default useAuthContext;
-
--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,14 +1,16 @@
 import { NavLink } from "react-router-dom";
 import { useFetchContext } from "../../contexts/FetchContext"
 import "./Landing.css"
+import useAuthContext from "../../contexts/AuthContext";
 export default function Landing() {
-    const { productState } = useFetchContext()
+    const { productState } = useFetchContext();
+    const { navigate } = useAuthContext();
     return (<div className="categories-container">
         <div className="categories">
             {
                 productState.arrCategories.map((item) => {
-                    const { _id, categoryName, image, description } = item;
-                    return (<div className="circle" key={_id}>
+                    const { _id, categoryName, image } = item;
+                    return (<div className="circle" key={_id} onClick={() => navigate("../pages/ProductPage/Product")}>
                         <img className="pic" src={`${image}`} width="100%" height="100%" alt="" />
                         <div className="heading">
                             <NavLink to="../pages/ProductPage/Product" className="heading-name">{categoryName}</NavLink>
