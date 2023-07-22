dee88641c18bb288c26d4bf7c0a40c5bf09fbdc1
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-22 16:52:36 +0530

    refactor: categories refactored

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -6,32 +6,35 @@ import { v4 as uuid } from "uuid";
  * */
 
 export const categories = [
-  
   {
     _id: uuid(),
     categoryName: "View all",
-    image:"https://www.edesk.com/wp-content/uploads/2021/03/find-trending-products-sell-ecommerce.png",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
-  },  
-  {
-    _id: uuid(),
-    categoryName: "Men's clothing",
-    image:"https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/halo/C/v1/Men_s_clothing._SS300_QL85_FMpng_.png",
+    image:
+      "https://www.edesk.com/wp-content/uploads/2021/03/find-trending-products-sell-ecommerce.png",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
   },
   {
     _id: uuid(),
-    categoryName: "Women's clothing",
-    image:"https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/halo/C/v1/Women_s_clothing._SS300_QL85_FMpng_.png",
+    categoryName: "Men",
+    image:
+      "https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/halo/C/v1/Men_s_clothing._SS300_QL85_FMpng_.png",
+    description:
+      "literature in the form of prose, especially novels, that describes imaginary events and people",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Women",
+    image:
+      "https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/halo/C/v1/Women_s_clothing._SS300_QL85_FMpng_.png",
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
   },
   {
     _id: uuid(),
-    categoryName: "Kids fashion",
-    image:"https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/halo/C/v1/03-Kids._SS300_QL85_FMpng_.png",
+    categoryName: "Kids",
+    image:
+      "https://m.media-amazon.com/images/G/31/img22/Fashion/AF/Newseason/revamp/halo/C/v1/03-Kids._SS300_QL85_FMpng_.png",
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
   },
