d87222027ac5df03d885eabd40c593477238f990
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-22 14:57:48 +0530

    style: authentication pages

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,15 +1,24 @@
 import { NavLink } from "react-router-dom";
 import useAuthContext from "../../contexts/AuthContext";
+import "./Authentication.css"
 
-export default function Login(){
-    const {setTokenToLocalStorage} = useAuthContext()
-    return(<div>
-        <h3>Login</h3>
-        <label>Email Address</label>
-        <input type="text" id="email" />
-        <label>Password</label>
-        <input type="text" id="password" />
-        <button onClick={()=>setTokenToLocalStorage(document.querySelector("#email").value,document.querySelector("#password").value)}>Login</button>
-        <NavLink to="/pages/Authentication/Signup">Create new account {"">""}</NavLink>
+export default function Login() {
+    const { setTokenToLocalStorage } = useAuthContext()
+    return (<div className="auth-container">
+        <div className="Brandname">
+            <h1>STREET</h1>
+        </div>
+        <div className="form">
+            <h2>Sign in</h2>
+            <label>Email Address</label>
+            <input type="text" id="email" />
+            <label>Password</label>
+            <input type="text" id="password" />
+            <button onClick={() => setTokenToLocalStorage(document.querySelector("#email").value, document.querySelector("#password").value)}>Login</button>
+            <NavLink to="/pages/Authentication/Signup">Don't have an account? Sign up {"" > ""}</NavLink>
+        </div>
+        <div className="form-footer">
+            <p>Ecommerce React App</p>
+        </div>
     </div>)
-}
+}   
--- src/pages/Authentication/Signup.jsx
+++ src/pages/Authentication/Signup.jsx
@@ -1,18 +1,27 @@
 import { NavLink } from "react-router-dom";
 import useAuthContext from "../../contexts/AuthContext";
 
-export default function Signup(){
-    const {createAcc} = useAuthContext()
-    return(<div>
-        <h3>Signup</h3>
-        <label>Name</label>
-        <input type="text"   id="username" />
-        <label>Email Address</label>
-        <input type="text"   id="mail"/>
-        <label>Password</label>
-        <input type="text"   id="password" />
-        <button onClick={()=>createAcc(document.querySelector("#mail").value,document.querySelector("#password").value,document.querySelector("#username").value)}>Create new account</button>
-        <NavLink to="/pages/Authentication/Login" >Already has an account {"">""}</NavLink>
+export default function Signup() {
+    const { createAcc } = useAuthContext()
+    return (<div className="auth-container">
+        <div className="Brandname">
+            <h1>STREET</h1>
+        </div>
+        <div className="form">
+            <h2>Sign up</h2>
+            <label>Name</label>
+            <input type="text" id="username" />
+            <label>Email Address</label>
+            <input type="text" id="mail" />
+            <label>Password</label>
+            <input type="text" id="password" />
+            <button onClick={() => createAcc(document.querySelector("#mail").value, document.querySelector("#password").value, document.querySelector("#username").value)}>Create new account</button>
+            <NavLink to="/pages/Authentication/Login" >
+                Already have an account? Sign in {"" > ""}</NavLink>
+        </div>
+        <div className="form-footer">
+            <p>Ecommerce React App</p>
+        </div>
     </div>)
 }
 
