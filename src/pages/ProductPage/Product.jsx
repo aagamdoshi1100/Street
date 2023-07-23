5b5adfa7f926e97ec1ae406486922ffb87245a4e
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-07-23 16:45:00 +0530

    style added and refactor

--- src/pages/ProductPage/Product.jsx
+++ src/pages/ProductPage/Product.jsx
@@ -24,14 +24,16 @@ export default function Product() {
                     const { _id, image, price, rating, title, Material } = item;
                     return (<div className="card" key={_id}>
                         <AiOutlineHeart className="wishlist" onClick={() => addToWishList(item)} />
-                        <img src={`${image}`} width="100%" height="180px" alt="" onClick={() => showClickedProduct(item)} />
-                        <p>{title}</p>
-                        <p style={{ textAlign: "center" }}>{rating}⭐</p>
-
-                        <p>Price: Rs {price}</p>
-
-                        {cartItem?.cartArray?.find((thing) => thing._id === _id) ? <button>Go to Cart</button> :
-                            <button className="card-btn" onClick={() => addToCart(item)}>Add to Cart</button>}
+                        <img src={`${image}`} width="100%" height="200px" alt="" onClick={() => showClickedProduct(item)} />
+                        <div className="description">
+                            <p className="productName">{title}</p>
+                            <h3>{rating}⭐</h3>
+                            <h3>Price: Rs {price}</h3>
+                            <div className="btns">
+                                {cartItem?.cartArray?.find((thing) => thing._id === _id) ? <button className="card-btn card-btn-single">Go to Cart</button> :
+                                    <button className="card-btn card-btn-single" onClick={() => addToCart(item)}>Add to Cart</button>}
+                            </div>
+                        </div>
                     </div>
 
                     )
