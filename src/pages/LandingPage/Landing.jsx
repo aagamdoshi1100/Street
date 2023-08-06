7aa338c938c8616e74015a5c835188321a71fc4b
Aagam Doshi <aagamdoshi1100@gmail.com>
2023-08-07 01:20:07 +0530

    refactor: landing page

--- src/pages/LandingPage/Landing.jsx
+++ src/pages/LandingPage/Landing.jsx
@@ -1,36 +1,50 @@
 import { NavLink } from "react-router-dom";
-import { useFetchContext } from "../../contexts/FetchContext"
+import { useFetchContext } from "../../contexts/FetchContext";
 import NavBar from "../../Components/NavBarPage/NavBar";
-import "./Landing.css"
+import "./Landing.css";
 
 export default function Landing() {
-    const { productState } = useFetchContext();
-    const { productDispatcher } = useFetchContext();
-    return (<div className="categories-container">
-        <NavBar />
-        <div className="categories">
-            {
-                productState.arrCategories.map((item) => {
-                    const { _id, categoryName, image } = item;
-                    return (<div className="circle" key={_id}  >
-                        <img className="pic" src={`${image}`} width="100%" height="100%" alt="" />
-                        <div className="heading">
-                            <NavLink
-                                to={`../pages/ProductPage/Product`}
-                                onClick={() => productDispatcher({ type: "TOGGLE_CATEGORY", payload: categoryName === "View all" ? ["Men", "Women", "Kids"] : categoryName })}
-                                className="heading-name"
-                            >
-                                {categoryName}
-                            </NavLink>
-                        </div>
-                    </div>
-                    )
-                })
-            }
-        </div>
-        <div className="offer-image">
-            <img src="https://shorturl.at/cxPT1" width="100%" height="100%" />
-        </div>
-
-    </div >)
+  const { productState } = useFetchContext();
+  const { productDispatcher } = useFetchContext();
+  return (
+    <div className="categories-container">
+      <NavBar />
+      <div className="categories">
+        {productState.arrCategories.map((item) => {
+          const { _id, categoryName, image } = item;
+          return (
+            <div className="circle" key={_id}>
+              <img
+                className="pic"
+                src={`${image}`}
+                width="100%"
+                height="100%"
+                alt=""
+              />
+              <div className="heading">
+                <NavLink
+                  to={`../pages/ProductPage/ProductPage`}
+                  onClick={() =>
+                    productDispatcher({
+                      type: "TOGGLE_CATEGORY",
+                      payload:
+                        categoryName === "View all"
+                          ? ["Men", "Women", "Kids"]
+                          : categoryName,
+                    })
+                  }
+                  className="heading-name"
+                >
+                  {categoryName}
+                </NavLink>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="offer-image">
+        <img src="https://shorturl.at/cxPT1" width="100%" height="100%" />
+      </div>
+    </div>
+  );
 }
